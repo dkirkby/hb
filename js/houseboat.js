56151ecd9be3df1210162b2f110eace792ffab83
David Kirkby <dkirkby@uci.edu>
2017-06-30 06:56:57 -0700

    Add jquery

--- js/houseboat.js
+++ js/houseboat.js
@@ -1,16 +1,42 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+function HouseBoat() {
+    console.log("Created new HouseBoat");
+}
 
-var hblength = 50;
-var hbwidth = 30;
+HouseBoat.prototype.initialize = function() {
+    console.log("HouseBoat.initialize()");
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 
-svg.selectAll("rect")
-        .data([0])
-    .enter().append("rect")
-        .attr("x", 0.5 * (width - hbwidth))
-        .attr("y", 0.5 * (height - hblength))
-        .attr("width", hbwidth)
-        .attr("height", hblength)
-        .style("fill", "black")
-        .style("opacity", 0.5);
+    /**
+    this.width = 30;
+    this.length = 50;
+    this.x = 0.5 * width;
+    this.y = 0.5 * height;
+    this.theta = 0.0;
+
+    svg.selectAll("rect")
+            .data([this])
+        .enter().append("rect")
+            .attr("x", 0.5 * (width - this.width))
+            .attr("y", 0.5 * (height - this.length))
+            .attr("width", this.width)
+            .attr("height", this.length)
+            .style("fill", "black")
+            .style("opacity", 0.5);
+    **/
+}
+
+$(function() {
+    // DOM is ready.
+    $( "p" ).text( "Initializing" );
+/*
+  });
+
+// Need scripts to load before going further.
+$(window).on("load", function(){
+*/
+    var hb = new HouseBoat();
+    hb.initialize();
+    $( "p" ).text( "Running" );
+});
