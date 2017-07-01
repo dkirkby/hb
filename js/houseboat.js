76820295f03dfd0941eb719d562e4f5b197a25c9
David Kirkby <dkirkby@uci.edu>
2017-06-30 17:09:03 -0700

    Smooth scrolling controls

--- js/houseboat.js
+++ js/houseboat.js
@@ -4,9 +4,9 @@ function HouseBoat() {
 
 HouseBoat.prototype.initialize = function() {
     console.log("HouseBoat.initialize()");
-    var svg = d3.select("svg"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+    this.svg = d3.select("svg");
+    this.window_width = +this.svg.attr("width");
+    this.window_height = +this.svg.attr("height");
     this.message = $("#message");
     // Capture arrow keypress events.
     this.right = false;
@@ -30,6 +30,12 @@ HouseBoat.prototype.initialize = function() {
             else if(code == 40) {
                 self.down = true;
             }
+            if(self.right || self.left) {
+                self.steering_display.attr("fill", "red");
+            }
+            if(self.up || self.down) {
+                self.throttle_display.attr("fill", "red");
+            }
         })
         .on("keyup", function() {
             var code = d3.event.keyCode;
@@ -45,17 +51,60 @@ HouseBoat.prototype.initialize = function() {
             else if(code == 40) {
                 self.down = false;
             }
+            if(!self.right && !self.left) {
+                self.steering_display.attr("fill", "green");
+            }
+            if(!self.up && !self.down) {
+                self.throttle_display.attr("fill", "green");
+            }
         });
+        // Initialize throttle and steering controls.
+        this.throttle = 0.0;
+        this.steering = 0.0;
+        var radius = 16;
+        this.throttle_display = this.svg.append("circle")
+            .attr("cx", self.window_width - radius)
+            .attr("cy", 0.5 * this.window_height - radius)
+            .attr("r", radius)
+            .attr("fill", "green");
+        this.steering_display = this.svg.append("circle")
+            .attr("cx", 0.5 * this.window_width - radius)
+            .attr("cy", self.window_height - radius)
+            .attr("r", radius)
+            .attr("fill", "green");
+        this.throttle_max = self.window_height - 2 * radius;
+        this.steering_max = self.window_width - 2 * radius;
 }
 
 HouseBoat.prototype.run = function() {
+    var ival = 50; // milliseconds
+    this.trans = d3.transition().duration(ival).ease(d3.easeLinear);
     // Start the interval timer.
     var self = this;
-    this.interval_id = setInterval(function() {
-        var turn_adjust = +1. * self.right - 1 * self.left;
+    //this.interval_id = setInterval(function() {
+    d3.interval(function(elapsed) {
         var throttle_adjust = +1. * self.up - 1. * self.down;
-        self.message.text("turn " + turn_adjust + " throttle " + throttle_adjust);
-    }, 200 /* ms */);
+        self.throttle += 0.01 * throttle_adjust;
+        if(self.throttle > 1.0) {
+            self.throttle = 1.0;
+        }
+        else if(self.throttle < -1.0) {
+            self.throttle = -1.0;
+        }
+        var steering_adjust = +1. * self.right - 1. * self.left;
+        self.steering += 0.01 * steering_adjust;
+        if(self.steering > 1.0) {
+            self.steering = 1.0;
+        }
+        else if(self.steering < -1.0) {
+            self.steering = -1.0;
+        }
+        //console.log('tick', elapsed, self.throttle, self.steering);
+        self.throttle_display //.transition(self.trans)
+            .attr("cy", 0.5 * (self.window_height - self.throttle_max * self.throttle));
+        self.steering_display //.transition(self.trans)
+            .attr("cx", 0.5 * (self.window_width + self.steering_max * self.steering));
+    }, ival);
 }
 
 $(function() {
