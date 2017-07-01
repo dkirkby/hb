a7a0135465fc62cf2bfc18e48bc95d4ef8becf3d
David Kirkby <dkirkby@uci.edu>
2017-06-30 21:56:55 -0700

    Add dynamics

--- js/houseboat.js
+++ js/houseboat.js
@@ -2,21 +2,6 @@ function HouseBoat() {
     console.log("Created new HouseBoat");
 }
 
-HouseBoat.prototype.draw_boat = function() {
-    this.boat
-        .attr("transform",
-            "translate(" + this.x + "," + this.y + ") rotate(" + (-this.theta) + ",0,0)");
-    if(Math.abs(this.throttle) < 0.05) {
-        wake_scale = 0.05;
-    }
-    else {
-        wake_scale = this.throttle;
-    }
-    this.wake
-        .attr("transform", "rotate(" + (-this.phi) + ") scale(" +
-            wake_scale + ")");
-}
-
 HouseBoat.prototype.initialize = function() {
     this.svg = d3.select("svg");
     this.window_width = +this.svg.attr("width");
@@ -28,6 +13,7 @@ HouseBoat.prototype.initialize = function() {
     this.phi = 0.0;
     this.vx = 0.0;
     this.vy = 0.0;
+    this.omega = 0.0;
     // Create boat's visual representation.
     this.boat = this.svg.append("g");
     this.boat.append("rect")
@@ -110,6 +96,70 @@ HouseBoat.prototype.initialize = function() {
             .attr("fill", "green");
         this.throttle_max = self.window_height - 2 * radius;
         this.steering_max = self.window_width - 2 * radius;
+        // Initialize constants.
+        this.deg2rad = Math.PI / 180;
+        this.max_thrust = 10.0;
+        this.drag_p = 0.1;
+        this.drag_t = 2.0;
+        this.drag_r = 0.2;
+        this.current_x = 0.0;
+        this.current_y = 0.0;
+        this.leverarm = 1.0;
+}
+
+HouseBoat.prototype.update_state = function() {
+    dt = 0.1;
+    // Initialize a unit vector aligned with the pontoons.
+    var theta = this.theta * this.deg2rad;
+    var nx = Math.cos(theta), ny = Math.sin(theta);
+    // Split the velocity (vx,vy) into components parallel
+    // and perpendicular to the pontoons.
+    var vpx = this.vx * nx + this.vy * ny;
+    var vpy = this.vy * nx - this.vx * ny;
+    var vtx = this.vx - vpx;
+    var vty = this.vy - vpy;
+    // Calculate the combined drag force.
+    var drag_x = -this.drag_p * vpx - this.drag_t * vtx;
+    var drag_y = -this.drag_p * vpy - this.drag_t * vty;
+    // Calculate the thrust force on the COM.
+    var phi = this.phi * this.deg2rad;
+    var angle = theta + phi;
+    var thrust_mag = this.max_thrust * this.throttle;
+    var thrust_x = thrust_mag * Math.cos(angle);
+    var thrust_y = thrust_mag * Math.sin(angle);
+    // Calculate the net force on the COM
+    var Fx = thrust_x + drag_x + this.current_x;
+    var Fy = thrust_y + drag_y + this.current_y;
+    // Update the position and bearing using the present motion.
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
+    this.theta += this.omega * dt;
+    //Update the COM velocity (assuming mass=1).
+    this.vx += Fx * dt;
+    this.vy += Fy * dt;
+    // Calculate the torque about the COM due to the thrust.
+    var tprop = -thrust_mag * this.leverarm * Math.sin(phi);
+    // Calculate the torque about the COM due to rotational drag.
+    var tdrag = -this.omega * this.drag_r;
+    // Calculate the net torque about the COM.
+    var tnet = tprop + tdrag;
+    // Update the angular velocity about the COM (assuming I=1).
+    this.omega += tnet * dt;
+}
+
+HouseBoat.prototype.draw_boat = function() {
+    this.boat
+        .attr("transform",
+            "translate(" + this.x + "," + this.y + ") rotate(" + (-this.theta) + ",0,0)");
+    if(Math.abs(this.throttle) < 0.05) {
+        wake_scale = 0.05;
+    }
+    else {
+        wake_scale = this.throttle;
+    }
+    this.wake
+        .attr("transform", "rotate(" + (-this.phi) + ") scale(" +
+            wake_scale + ")");
 }
 
 HouseBoat.prototype.run = function() {
@@ -140,6 +190,7 @@ HouseBoat.prototype.run = function() {
         self.steering_display //.transition(self.trans)
             .attr("cx", 0.5 * (self.window_width + self.steering_max * self.steering));
         self.phi = 90.0 * self.steering;
+        self.update_state();
         self.draw_boat();
     }, ival);
 }
